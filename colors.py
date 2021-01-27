2532fa674cc7b7ea0449cae8ddac46086002257c
HighTemplar-wjiang <weiweijiangcn@gmail.com>
2021-01-27 16:41:56 +1100

    add colors

---
+++ colors.py
@@ -0,0 +1,16 @@
+# Created on 20210127 by HighTemplar-wjiang
+# Some colors for good looking plots. 
+
+# Colors for points, lines.
+color_list_curves = [
+    "#cf625b", # Red-like,
+    "#51a8cf", # Blue-like,
+    "#50bf57", # Green-like
+]
+
+# Colors for fill-in. 
+color_list_fillins = [
+    "#64c9f5", # Light blue
+    "#f57e76", # Light red
+]
+
